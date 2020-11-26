f5011a48146b6dff4fd18ed7cada649183b8ec8a
wancocoding <ergal@163.com>
2020-11-26 18:21:52 +0800

    format python code

--- python3/pi2md/__init__.py
+++ python3/pi2md/__init__.py
@@ -1,3 +1,10 @@
-from .pi2md import download_image, detect_picgo_api_server, \
-    copy_file, remove_file, picgo_upload, save_clipboard, \
-    simple_hash_text, detect_file
+from pi2md import (
+    download_image,
+    detect_picgo_api_server,
+    copy_file,
+    remove_file,
+    picgo_upload,
+    save_clipboard,
+    simple_hash_text,
+    detect_file,
+)
